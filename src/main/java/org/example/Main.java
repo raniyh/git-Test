e5e024a0e00762844d7dbe8dadc8471d8c0021db
raniyh <105971099+raniyh@users.noreply.github.com>
2024-06-09 12:30:17 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("matser1-test");
         System.out.println("hot-test");
         System.out.println("push-Test");
+        System.out.println("pull-Test");
 
     }
 }
